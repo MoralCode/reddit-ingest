3ff4310f64e39be73170916fb6e3ded5cc151722
led9628 <led9628@rit.edu>
2023-02-25 18:24:09 -0500

    Made engine global variable

--- SQLiteConnection.py
+++ SQLiteConnection.py
@@ -1,16 +1,18 @@
 from sqlalchemy import create_engine
 from Tables import mapper_registry
 
+# Create a database engine
+engine = create_engine('sqlite:///my_database.db')
+
 def main():
     try:
-        # Create a database engine
-        engine = create_engine('sqlite:///my_database.db')
 
         # Connect to the database
         connection = engine.connect()
 
         #Add actual functionality from other files here (potentially a driver class)
         mapper_registry.metadata.create_all(engine)
+
 
     except Exception as e:
 
