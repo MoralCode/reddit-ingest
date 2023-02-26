c247577e2fc67913a5b4f1633e01a875bcc90648
led9628 <led9628@rit.edu>
2023-02-25 19:50:36 -0500

    added session

--- RedditScraping.py
+++ RedditScraping.py
@@ -4,9 +4,10 @@ import requests
 from os import getenv
 from dotenv import load_dotenv
 import praw
-import SQLiteConnection
+from SQLiteConnection import engine
 from Tables import Vibes
 from praw.models import Submission
+from sqlalchemy.orm import sessionmaker
 
 #set up the parser
 parser = argparse.ArgumentParser()
@@ -22,13 +23,18 @@ reddit = praw.Reddit(
     client_id = getenv("client_id"),
     client_secret = getenv("secret_key"),
     user_agent = "WTFRIT",
-
 )
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 if args.link is None:
     for submission in reddit.subreddit("rit").hot(limit=50):
         if submission.selftext != "":
-            post = Vibes(title = submission.title, contents=submission.selftext, upvotes=submission.score, source_url=submission.url, last_updated= datetime.datetime.fromtimestamp(0, submission.created_utc))
+            post = Vibes(title = submission.title, contents=submission.selftext, upvotes=submission.score, source_url=submission.url, last_updated= datetime.datetime.fromtimestamp(int(submission.created_utc)))
+            session.add(post)
 else:
     submission = Submission(reddit=reddit, url=args.link)
-    post = Vibes(title = submission.title, contents=submission.selftext, upvotes=submission.score, source_url= submission.url, last_updated= datetime.datetime.fromtimestamp(submission.created_utc))
+    post = Vibes(title = submission.title, contents=submission.selftext, upvotes=submission.score, source_url= submission.url, last_updated= datetime.datetime.fromtimestamp(int(submission.created_utc)))
+    session.add(post)
+session.commit()
